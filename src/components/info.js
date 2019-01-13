48d647d3003d607f94e7039808a0d51d92a74ff8
vosem <trefoil2@ukr.net>
2019-01-13 14:28:58 +0200

    creating first component

---
+++ src/components/info.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Info extends React.Component {
+	render() {
+		return (
+			<div>
+				<h2>Weather application</h2>
+				<p>Get weather!</p>
+			</div>
+		);
+	}
+}
+
+export default Info;
