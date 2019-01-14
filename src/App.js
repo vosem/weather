53dd47b6072afbb736d3f1be6ee4b28ebb6fe71d
vosem <trefoil2@ukr.net>
2019-01-14 10:10:15 +0200

    further development

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import Info from "./components/info";
+import Form from "./components/form";
+import Weather from "./components/weather";
 import "./App.css";
 
+const API_KEY = "8f3402da9f52d031b9f9d6d8727b3798";
+
 class App extends Component {
+
+  gettingWaether = async () => {
+    const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Kiev,ua&appid=${API_KEY}&units=metric`);
+    const data = api_url.json();
+    console.log(data);
+  }
+
   render() {
     return (
       <div class="App">
         <Info />
+        <Form />
+        <Weather />
       </div>
     );
   }
